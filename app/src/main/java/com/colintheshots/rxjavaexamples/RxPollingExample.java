1519c4113acd5dd3d1ab31bc6494bd2d3e0d06eb
Colin Lee <colin.lee@mentormate.com>
2014-11-20 16:34:21 -0800

    Add Rx schedulers polling example.

---
+++ app/src/main/java/com/colintheshots/rxjavaexamples/RxPollingExample.java
@@ -0,0 +1,56 @@
+package com.colintheshots.rxjavaexamples;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Rx scheduler polling example.
+ *
+ * 1. Make the worker and observable end after n number of iterations.
+ * 2. Complete if the seconds are equal to :00.
+ */
+public class RxPollingExample {
+
+    private static final int INITIAL_DELAY = 1000;
+    private static final int POLLING_INTERVAL = 1000;
+    private static final String DATE_FORMAT = "h:mm:ss a";
+
+    public static void main(String[] args) {
+        Observable.create(new Observable.OnSubscribe<Calendar>() {
+            @Override
+            public void call(final Subscriber<? super Calendar> subscriber) {
+                Schedulers.immediate().createWorker()
+                        .schedulePeriodically(new Action0() {
+                            @Override
+                            public void call() {
+                                subscriber.onNext(Calendar.getInstance(Locale.US));
+                            }
+                        }, INITIAL_DELAY, POLLING_INTERVAL, TimeUnit.MILLISECONDS);
+            }
+        })
+            .take(10)
+            .observeOn(Schedulers.io())
+            .subscribe(new Action1<Calendar>() {
+                @Override
+                public void call(Calendar calendar) {
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT);
+                    String time = simpleDateFormat.format(calendar.getTime());
+                    System.out.println(time);
+                }
+            }, new Action1<Throwable>() {
+                @Override
+                public void call(Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            });
+
+    }
+}
